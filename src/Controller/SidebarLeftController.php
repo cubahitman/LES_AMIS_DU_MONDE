8944c62306186924c21230f18779c26f89e4b1ab
cubahitman <rquesadarad@yahoo.fr>
2023-06-30 17:11:22 +0200

    formulaire registration

---
+++ src/Controller/SidebarLeftController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SidebarLeftController extends AbstractController
+{
+    #[Route('/sidebar/left', name: 'app_sidebar_left')]
+    public function index(): Response
+    {
+        return $this->render('sidebar_left/index.html.twig', [
+            'controller_name' => 'SidebarLeftController',
+        ]);
+    }
+}
